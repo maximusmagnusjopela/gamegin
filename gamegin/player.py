6e2bdcc0b4ad1b1a93b74814eab8e22a20c2aab8
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2015-08-23 15:05:37 -0400

    Adding HumanPlayer implementation

--- gamegin/player.py
+++ gamegin/player.py
@@ -3,6 +3,8 @@
 
 import base
 import random
+import copy
+import sys
 
 class RandomPlayer(base.Player):
     """ Player that give random values to all the possible states """
@@ -16,4 +18,26 @@ class RandomPlayer(base.Player):
 
     def score(self,state):
         return random.random()
+
+class HumanPlayer(base.Player):
+    """ Ask a human to score the moves. """
+
+    def __init__(self, player_symbol):
+        super().__init__(player_symbol)
+        return
+
+    def __eq__(self, other):
+        return self.player_symbol == other.player_symbol
+
+    def play(self, state):
+        """ Returns the state choosen by the user. """
+        prompt = state.prompt_state_str()
+
+        while True:
+            raw_index = input(prompt)
+            choosen_state = state.transition(raw_index)
+            if choosen_state:
+                return choosen_state
+            else:
+                print('invalid state transition. Please try again',file=sys.stderr)
 
